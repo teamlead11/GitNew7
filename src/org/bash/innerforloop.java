8ccf44169336a73d2c6dd4172dc121e03ce8a5b1
SIRISHACHANDRA <gsirisha.444@gmail.com>
2019-06-14 01:56:22 -0400

    added or not

---
+++ src/org/bash/innerforloop.java
@@ -0,0 +1,14 @@
+package computer.org;
+
+public class innerforloop{
+	public static void main(String[] args) {
+		
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=6;j++) {
+			System.out.println(i);
+		}
+		System.out.println();
+	}
+
+}
+}
