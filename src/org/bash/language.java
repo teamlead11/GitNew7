633448a96b36bc99333f6a238b7781d22f78ae96
Ramadevi1704 <ramadevi.saba@gmail.com>
2019-06-14 11:28:16 +0530

    added program

---
+++ src/org/bash/language.java
@@ -0,0 +1,15 @@
+package org.bash;
+
+public class language {
+			public void northIndia() {
+	System.out.println("north");
+		}
+		public static void main(String[] args) {
+					language d=new language();
+		d.northIndia();
+			
+	}
+
+	}
+
+
