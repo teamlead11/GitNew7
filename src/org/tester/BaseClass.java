bddd4a89d5d9f8eed7475a9e6544f46a540d2d46
praneethaedara <praneethaedara@gmail.com>
2019-06-17 13:12:00 +0530

    add baseclass

--- src/org/tester/BaseClass.java
+++ src/org/tester/BaseClass.java
@@ -1,15 +1,19 @@
 package org.tester;
 
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
-		int a=12;
-		if(a%2==1) {
-System.out.println("even number");
-		}
-else {
-System.out.println("odd number");	
-}
+	
+			String original, reverse = "";
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter a string to check if it is a palindrome");
+			original = in.nextLine();
+			int length = original.length();
+			for ( int i = length - 1; i >= 0; i-- )
+			reverse = reverse + original.charAt(i);
+			if (original.equals(reverse))
+			System.out.println("Entered string is a palindrome.");
+			}
 	}
-
-}
