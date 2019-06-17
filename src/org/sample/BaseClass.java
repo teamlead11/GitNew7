aa90c34bec344827c3d389fddae1f3fc393e69b1
SIRISHACHANDRA <gsirisha.444@gmail.com>
2019-06-17 02:32:28 -0400

    palindrome code

---
+++ src/org/sample/BaseClass.java
@@ -0,0 +1,22 @@
+package org.sample;
+
+public class BaseClass {
+	public void stringPalindromeSirisha(){
+		System.out.println("This is My Palindrome");
+		
+	}
+public static void main(String[] args) {
+	String rev="";
+	String s="DAD";
+	for(int i=s.length()-1;i>=0;i--) {
+		rev=rev+s.charAt(i);
+		
+	}
+	if(s.equals(rev)) {
+		System.out.println("String Is Palindrome");
+	}
+	else {
+		System.out.println("String Is Not Palindrome");
+	}
+}
+}
