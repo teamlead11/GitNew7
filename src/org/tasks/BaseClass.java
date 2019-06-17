6aa3bf13c4c7ffef97493003fdd74978148c5aae
praveenavarma <praveena1139@gmail.com>
2019-06-17 12:08:45 +0530

    added palindrom program

---
+++ src/org/tasks/BaseClass.java
@@ -0,0 +1,21 @@
+package org.tasks;
+
+public class BaseClass {
+	public void stringPalindromePraveena() {
+		System.out.println("this is my paliindrome program");
+	}
+	public static void main(String[] args) {
+		String rev="";
+		String s="mom";
+		for(int i=s.length()-1;i>=0;i--) {
+			rev=rev+s.charAt(i);
+		}
+		if(s.equals(rev)) {
+			System.out.println("string is palindrome");
+		}
+		else {
+			System.out.println("string is not palindrome");
+		}
+	}
+
+}
