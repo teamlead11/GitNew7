5f41cbd80740dc2bb22a9d91a6de3e3c29e3a60b
kishorerider91 <brillakishore@gmail.com>
2019-06-17 12:57:02 +0530

    modified deletde

--- src/org/client/Class2.java
+++ src/org/client/Class2.java
@@ -7,6 +7,10 @@ public static void main(String[] args) {
 	System.out.println(s.length());
 	String[] s1=s.split(" ");
 System.out.println(s1.length);
+String d="praveena varma";
+System.out.println(s.length());
+String[] d1=s.split(" ");
+System.out.println(s1.length);
 
 
 
