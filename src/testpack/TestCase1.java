bfbbf6a0a170d5222c6e359a0d59a6e4c9f3342f
K <karthick-raja.k@dxc.com>
2019-06-17 11:46:00 +0530

    done

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -4,7 +4,7 @@ public class TestCase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my test case1");
+		System.out.println("this is my test case2");
 	}
 
 }
