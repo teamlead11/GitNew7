7d8d41c12eaa3febe65816abfb7a77d7c4f41ed2
swathi5511 <dswathi5511@gmail.com>
2019-06-17 17:29:31 +0530

    new project

---
+++ src/org/cts/testlogin/CorseDetails.java
@@ -0,0 +1,22 @@
+package org.cts.testlogin;
+
+public class CorseDetails {
+	
+	public void javaCorse() {
+		
+		System.out.println(" java corse is started ");
+	}
+	public void oracleCorse() {
+		
+		System.out.println(" oracle corse is not started ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CorseDetails cd = new CorseDetails();
+		cd.javaCorse();
+		cd.oracleCorse();
+	}
+
+}
---
+++ src/org/cts/testlogin/PhoneInfo.java
@@ -0,0 +1,37 @@
+package org.cts.testlogin;
+
+public class PhoneInfo {
+	
+	public void phoneName() {
+		
+		System.out.println("My phone no is 764345364576 ");
+	}
+	public void phoneMieiNum() {
+		
+		System.out.println("My phone no is 6575765675 ");
+	}
+	public void Camera() {
+		
+		System.out.println("My Camera is 16mp ");
+	}
+	public void storage() {
+	
+	System.out.println("My storage  is 65GB ");
+	}
+	public void osName() {
+	
+	System.out.println("My osName no  android ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PhoneInfo p=new PhoneInfo();
+		
+		p.phoneMieiNum();
+		p.phoneName();
+		p.Camera();
+		p.storage();
+		p.osName();
+	}
+
+}
