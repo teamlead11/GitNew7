28fc755868538d0a1eba76363fc65e1b7b502396
praneethaedara <praneethaedara@gmail.com>
2019-06-18 12:31:21 +0530

    base new

--- src/org/tester/BaseClass.java
+++ src/org/tester/BaseClass.java
@@ -2,9 +2,24 @@ package org.tester;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		public void PalindromePraneetha() {
+			String name = "praneetha";
+			String res = "";
+			for (int i = name.length() - 1; i >= 0; i--) {
+				char ch = name.charAt(i);
+				res = res + ch;
+			}
+			if (res.equals(name)) {
+				System.out.println("Palindrome string");
+			} 
+			else {
+				System.out.println("Not palindrome string");
+	}
+		}	
+		public static void main(String[] args) {
+			BaseClass b=new BaseClass();
+			b.PalindromePraneetha();
+			
 	}
 
 }
