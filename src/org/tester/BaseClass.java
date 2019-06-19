9be816cb8220490e4d539ac8a57370c87876d1c9
praneethaedara <praneethaedara@gmail.com>
2019-06-19 17:10:52 +0530

    modified1

--- src/org/tester/BaseClass.java
+++ src/org/tester/BaseClass.java
@@ -16,7 +16,20 @@ public class BaseClass {
 		System.out.println("Not Palindrome");
 	}
 	}
-	
+	public void practice() {
+		String name = "pract";
+		String res = "";
+		for (int i=name.length()-1;i>=0;i--) {
+			char ch=name.charAt(i);
+			res = res+ch;
+			
+		}
+	if(res.equals(name)) {
+		System.out.println("Palindrome");
+	}else {
+		System.out.println("Not Palindrome");
+	}
+	}
 
 	public static void main(String[] args) {
 BaseClass B=new BaseClass();
