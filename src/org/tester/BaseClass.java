b43c312397846f3f204c0dbfb9975365a00ccef9
praneethaedara <praneethaedara@gmail.com>
2019-06-19 15:58:17 +0530

    modify

--- src/org/tester/BaseClass.java
+++ src/org/tester/BaseClass.java
@@ -12,13 +12,11 @@ public class BaseClass {
 		}
 	if(res.equals(name)) {
 		System.out.println("Palindrome");
-	}else {
+	}
+	else {
 		System.out.println("Not Palindrome");
 	}
 	}
-	
-
-<<<<<<< HEAD
 		public void PalindromePraneetha() {
 			String name = "praneetha";
 			String res = "";
@@ -31,18 +29,10 @@ public class BaseClass {
 			} 
 			else {
 				System.out.println("Not palindrome string");
-	}
-		}	
-		public static void main(String[] args) {
-			BaseClass b=new BaseClass();
-			b.PalindromePraneetha();
-			
-=======
-	public static void main(String[] args) {
-BaseClass B=new BaseClass();
-B.Arun();
-
->>>>>>> 02c3d9e3c4924e1f3f98d4cc9ad0cc0b6d4d680a
-	}
-
-}
+			}
+		}
+			public static void main(String[] args) {
+				BaseClass B=new BaseClass();
+				B.Arun();
+			B.PalindromePraneetha();
+			}}		
