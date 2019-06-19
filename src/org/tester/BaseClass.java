eb410786dde91c24f2991c3e77fa10c0c83ff82c
praneethaedara <praneethaedara@gmail.com>
2019-06-19 17:13:40 +0530

    modified3

--- src/org/tester/BaseClass.java
+++ src/org/tester/BaseClass.java
@@ -2,20 +2,6 @@ package org.tester;
 
 public class BaseClass {
 	
-	public void Arun() {
-		String name = "Arun";
-		String res = "";
-		for (int i=name.length()-1;i>=0;i--) {
-			char ch=name.charAt(i);
-			res = res+ch;
-			
-		}
-	if(res.equals(name)) {
-		System.out.println("Palindrome");
-	}else {
-		System.out.println("Not Palindrome");
-	}
-	}
 	public void practice() {
 		String name = "pract";
 		String res = "";
@@ -30,10 +16,10 @@ public class BaseClass {
 		System.out.println("Not Palindrome");
 	}
 	}
-
+	
 	public static void main(String[] args) {
 BaseClass B=new BaseClass();
-B.Arun();
+B.practice();
 
 	}
 
